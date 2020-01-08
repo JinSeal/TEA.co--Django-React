cb4314361bee18f28a0fa4a2202fa9a9f3260fdf
JinSeal <1183958@gmail.com>
2020-01-08 15:43:21 +1100

    2 days' learning

---
+++ core/api/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from .views import (
+    ItemListView,
+    AddToCartView,
+    OrderDetailView,
+    PaymentView
+)
+
+urlpatterns = [
+    path('products/', ItemListView.as_view(), name='product-list'),
+    path('add-to-cart/', AddToCartView.as_view(), name='add-to-cart'),
+    path('order-summary/', OrderDetailView.as_view(), name='order-summary'),
+    path('handle-payment/', PaymentView.as_view(), name='handle-payment')
+]
